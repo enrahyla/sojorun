64a3153cd16cb0ad77369882c268fcdfad802282
enrahyla <A.D.Hyland@2009.ljmu.ac.uk>
2013-02-12 15:29:14 +0000

    update
    tags added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Sojourn::Application.routes.draw do
 
+  get "tags/index"
+
+  get "tags/show"
+
   get "welcome/about"
 
   get "welcome/contact"
 
   # root :to => 'welcome#index'
   resources :articles
+
+  resources :tags
 end
