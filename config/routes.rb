76a5c2bf62c57d78abcf2255b1b87c449e3c9074
enrahyla <A.D.Hyland@2009.ljmu.ac.uk>
2013-02-19 15:09:07 +0000

    update
    main sojourn articles site

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Sojourn::Application.routes.draw do
-
-  get "tags/index"
-
-  get "tags/show"
-
-  get "welcome/about"
-
-  get "welcome/contact"
-
-  # root :to => 'welcome#index'
+  root :to => 'articles#index'
   resources :articles
-
   resources :tags
 end
